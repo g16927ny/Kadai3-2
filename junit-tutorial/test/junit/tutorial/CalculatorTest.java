92f19bb59779b7ea9691cb604d3ecf4bc582fb87
g16927ny <g16927ny@gm.tsuda.ac.jp>
2018-05-24 10:08:07 +0900

    GitHubにプッシュされていないことに昨日気づきました。これからはGitHubにpushされているかしっかり確認したいと思います

---
+++ junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+	public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
